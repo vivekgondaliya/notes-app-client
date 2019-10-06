9dbf7d689a40984c0d7553fbcaae5d620adc7131
vivekgondaliya <vivekgondaliyaatwork@gmail.com>
2019-10-06 06:16:48 +0530

    added navlinks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { Navbar } from "react-bootstrap";
+import { Nav, Navbar, NavItem } from "react-bootstrap";
 import "./App.css";
 import Routes from "./Routes";
 
@@ -15,11 +15,18 @@ class App extends Component {
             </Navbar.Brand>
             <Navbar.Toggle />
           </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <NavItem href="/signup">Signup</NavItem>
+              <NavItem href="/login">Login</NavItem>
+            </Nav>
+          </Navbar.Collapse>
         </Navbar>
         <Routes />
       </div>
     );
   }
+  
 }
 
 export default App;
